3c2aeca6c532d4cdfdc415d267831240cde70e4c
¨Andrea <andreafuentes@uadec.edu.mx.com>
2023-03-21 17:54:49 -0600

    inss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { TicketComponent } from './components/ticket/ticket.component';
 
 
 const routes: Routes = [
-  { path: 'ticket', component: TicketComponent }
+  { path: 'ticket', component: TicketComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
